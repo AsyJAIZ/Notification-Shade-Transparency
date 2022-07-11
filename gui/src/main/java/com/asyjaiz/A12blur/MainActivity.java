aec2852b235c3c95102d63b3ba2f560dc8056ac9
unknown <2007andrylavr@gmail.com>
2022-07-11 04:40:33 +0200

    first commit

---
+++ gui/src/main/java/com/asyjaiz/A12blur/MainActivity.java
@@ -0,0 +1,139 @@
+package com.asyjaiz.A12blur;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.slider.Slider;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+    public static final String[] scrimList = {"mScrimBehind", "mNotificationsScrim", "mScrimInFront", "mScrimForBubble"};
+    public static final Float maxBlurDefault = 23f;
+    SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {prefs = getPreferences(MODE_WORLD_READABLE);}
+        catch (SecurityException ignore) {
+            prefs = getPreferences(MODE_PRIVATE);
+            Snackbar snackbar = Snackbar.make(findViewById(R.id.MainActivity), R.string.activateWarning, 3000);
+            final View snackBarView = snackbar != null ? snackbar.getView() : null;
+            final TextView tv = snackbar != null? snackBarView.findViewById(com.google.android.material.R.id.snackbar_text) : null;
+            tv.setAutoLinkMask(Linkify.ALL);
+            tv.setMovementMethod(LinkMovementMethod.getInstance());
+            snackbar.show();
+
+        }
+
+        fadeIn.setDuration(250);
+
+        Slider maxBlur = findViewById(R.id.maxBlur);
+        EditText maxBlurValue = findViewById(R.id.maxBlurValue);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list, scrimList);
+        TextInputLayout scrim = findViewById(R.id.selScrim);
+        AutoCompleteTextView selector = ((AutoCompleteTextView) scrim.getEditText());
+        assert selector != null;
+        selector.setAdapter(adapter);
+
+        Slider alphaValue = findViewById(R.id.alphaValue);
+
+        maxBlur.addOnChangeListener((slider, value, fromUser) -> maxBlurValue.setText(String.valueOf(Math.round(value))));
+        maxBlur.setValue(prefs.getFloat("max_window_blur_radius", maxBlurDefault));
+        final boolean[] warn = {false};
+        maxBlurValue.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @SuppressLint("ResourceType")
+            @Override
+            public void afterTextChanged(Editable s) {
+                int value;
+
+                if (s.toString().equals(""))
+                    value = 0;
+                else value = Integer.parseInt(s.toString());
+
+                if (!(prefs.getFloat("max_window_blur_radius", 23f) == (float) value))
+                {
+                    if (value > 23) {
+                        if (!warn[0]) {
+                            maxBlur.setValueTo(150f);
+                            Toast.makeText(MainActivity.this, R.string.maxBlurWarning, LENGTH_SHORT).show();
+                            warn[0] = true;
+                        }
+                    }
+
+                    if (value < 0) {
+                        maxBlurValue.setText(String.valueOf(0));
+                        maxBlur.setValue(0);
+                    }
+                    else if (value > 150) {
+                        maxBlurValue.setText(String.valueOf(150));
+                        maxBlur.setValue(150);
+                    }
+                    else maxBlur.setValue(value);
+
+                    prefs.edit().putFloat("max_window_blur_radius", value).apply();
+                }
+            }
+        });
+        selector.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (alphaValue.getVisibility() == View.INVISIBLE) {
+                    fadeIn.setFillAfter(true);
+                    alphaValue.setVisibility(View.VISIBLE);
+                    alphaValue.startAnimation(fadeIn);
+                }
+
+                alphaValue.setValue(prefs.getFloat(s.toString(), 1.0f));
+            }
+        });
+
+        alphaValue.addOnChangeListener((slider, value, fromUser) -> {
+            if (fromUser)
+                prefs.edit().putFloat(selector.getText().toString(), value).apply();
+        });
+
+    }
+}
---
+++ gui/src/main/java/com/asyjaiz/A12blur/Replace.java
@@ -0,0 +1,107 @@
+package com.asyjaiz.A12blur;
+
+import static com.asyjaiz.A12blur.MainActivity.maxBlurDefault;
+import static com.asyjaiz.A12blur.MainActivity.scrimList;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import android.content.res.XResources;
+import android.os.Build;
+import android.util.TypedValue;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
+    String pkg = "com.android.systemui";
+    static XSharedPreferences prefs;
+    //Context context;
+
+    //public Replace(Context context) {
+        //this.context = context;
+
+    //}
+
+    public boolean versionCheck() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.S;
+    }
+
+    ArrayList<String> done = new ArrayList<>();
+
+    public void apply(XC_MethodHook.MethodHookParam param, Class<?> findClass) throws Throwable {
+        done.clear();
+        for (String scrim : scrimList) {
+            if (!XposedHelpers.findField(findClass, scrim).get(param.thisObject).equals(param.args[0]))
+                continue;
+
+            if (done.contains(scrim))
+                continue;
+
+            //param.args[1] = (Float) param.args[1] * (Float) entry.getValue();
+            param.args[1] = prefs.getFloat(scrim, 1.0f);
+            //XposedBridge.log(scrim + " " + param.args[1]);
+            param.args[2] = -16777216;
+            done.add(scrim);
+        }
+    }
+
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+        prefs = new XSharedPreferences(startupParam.modulePath, "MainActivity");
+        //prefs.getFile().setReadable(true);
+        //prefs = new XSharedPreferences(startupParam.modulePath, "MainActivity");
+        prefs = prefs.getFile().canRead() ? prefs : null;
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        if (!resparam.packageName.equals(pkg) || versionCheck())
+            return;
+        if (prefs == null) {
+            //XposedBridge.log("Shared Preferences using may produce NullPointerException.");
+            prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID, "MainActivity");
+            if (!prefs.getFile().canRead()) {
+                //XposedBridge.log("Still can't read. Return");
+                return;
+            }
+            //else XposedBridge.log("Pass");
+        }
+
+        //XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
+        resparam.res.setReplacement(pkg, "dimen", "max_window_blur_radius", new XResources.DimensionReplacement(prefs.getFloat("max_window_blur_radius", maxBlurDefault), TypedValue.COMPLEX_UNIT_PX));
+    }
+
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals(pkg) || versionCheck())
+            return;
+        if (prefs == null) {
+            //XposedBridge.log("Shared Preferences using may produce NullPointerException.");
+            prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID, "MainActivity");
+            if (!prefs.getFile().canRead()) {
+                //XposedBridge.log("Still can't read. Return");
+                return;
+            }
+            //else XposedBridge.log("Pass");
+        }
+
+        final Class<?> findClass = XposedHelpers.findClass(pkg + ".statusbar.phone.ScrimController", lpparam.classLoader);
+        //Field[] scrimView = {XposedHelpers.findField(findClass, scrimList[0]), XposedHelpers.findField(findClass, scrimList[1]), XposedHelpers.findField(findClass, scrimList[2]), XposedHelpers.findField(findClass, scrimList[3])};
+
+        findAndHookMethod(findClass, "updateScrimColor", View.class, float.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                apply(param, findClass);
+            }
+        });
+    }
+}
+
