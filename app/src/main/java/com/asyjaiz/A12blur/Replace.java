ff13a315439b6f90ea6dca5ff720e2120de28218
AsyJAIZ <2007andrylavr@gmail.com>
2022-07-22 02:42:15 +0200

    add tint boolean, try to get blur support

--- app/src/main/java/com/asyjaiz/A12blur/Replace.java
+++ app/src/main/java/com/asyjaiz/A12blur/Replace.java
@@ -1,45 +1,57 @@
 package com.asyjaiz.A12blur;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.findField;
 
+import android.content.res.XModuleResources;
 import android.os.Build;
 import android.view.View;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-// public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
+public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
 
-public class Replace implements IXposedHookLoadPackage {
+//public class Replace implements IXposedHookLoadPackage {
     String pkg = "com.android.systemui";
-    //private static String MODULE_PATH = null;
+    private final boolean tint = true;
+    private String MODULE_PATH;
 
     public boolean versionCheck() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S;
     }
 
-    //@Override
-    //public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
-        //MODULE_PATH = startupParam.modulePath;
-    //}
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+        MODULE_PATH = startupParam.modulePath;
+        //XResources.setSystemWideReplacement("com.android.internal", "bool", "config_avoidGfxAccel", true);
+    }
 
-    //@Override
-    //public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
-        //if (!resparam.packageName.equals(pkg) || !versionCheck())
-            //return;
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        if (!resparam.packageName.equals(pkg) || !versionCheck())
+            return;
 
-        //XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
-        //resparam.res.setReplacement(pkg, "dimen", "max_window_blur_radius", modRes.fwd(R.dimen.max_window_blur_radius));
-    //}
+        XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
+        //posedBridge.log("yass intremal;");
+        //resparam.res.setReplacement("com.android.internal", "bool", "config_avoidGfxAccel", true);
+        resparam.res.setReplacement(pkg, "dimen", "max_window_blur_radius", modRes.fwd(R.dimen.max_window_blur_radius));
+    }
 
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals(pkg) || !versionCheck())
             return;
 
         final Class<?> findClass = XposedHelpers.findClass(pkg + ".statusbar.phone.ScrimController", lpparam.classLoader);
+        final Class<?> blurClass = XposedHelpers.findClass(pkg + ".statusbar.BlurUtils", lpparam.classLoader);
+
+
+        //findAndHookMethod(blurClass, "supportsBlursOnWindows", XC_MethodReplacement.returnConstant(true));
+        final float[] value = {0.0f};
 
         findAndHookMethod(findClass, "updateScrimColor", View.class, float.class, int.class, new XC_MethodHook() {
             @Override
@@ -47,9 +59,22 @@ public class Replace implements IXposedHookLoadPackage {
                 if (!XposedHelpers.findField(findClass, "mNotificationsScrim").get(param.thisObject).equals(param.args[0]))
                     return;
 
-                param.args[1] = (Float) param.args[1] * 0.0f;
-                param.args[2] = -16777216;
+                if (tint && (value[0] == 0.0f))
+                    return;
 
+                param.args[1] = (Float) param.args[1] * value[0];
+                //param.args[2] = -16777216;
+
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                //Object eins = XposedHelpers.callStaticMethod(blurClass, "supportsBlursOnWindows");
+                //Object zwei = blurClass.
+                if (tint && (value[0] == 0.0f)) {
+                    if (true) { value[0] = 0.54f; }
+                    else { value[0] = 0.85f; }
+                }
             }
         });
     }
