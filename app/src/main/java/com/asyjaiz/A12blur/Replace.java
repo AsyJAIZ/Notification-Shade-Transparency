0de84c6d0cbceaedca742c1acd701d0649d0f14e
AsyJAIZ <2007andrylavr@gmail.com>
2023-03-19 17:09:05 +0100

    fix fix fix

--- app/src/main/java/com/asyjaiz/A12blur/Replace.java
+++ app/src/main/java/com/asyjaiz/A12blur/Replace.java
@@ -27,6 +27,8 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
     boolean lowRam;
     boolean disableBlur;
 
+    Object[] parameterTypesAndCallback;
+
     public static Boolean read(String propName) {
         Process process = null;
         BufferedReader bufferedReader = null;
@@ -35,9 +37,13 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
             process = new ProcessBuilder().command("/system/bin/getprop", propName).start();
             bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line = bufferedReader.readLine();
-            return line.contains("true") || line.contains("1");
+            boolean contains = line.contains("true") || line.contains("1");
+            if (BuildConfig.DEBUG) XposedBridge.log("Check for " + propName + " returned " +
+                    line +
+                    (contains ? " and contains a success value" : " and doesn't contain a success value"));
+            return contains;
         } catch (Exception e) {
-            XposedBridge.log(e);
+            XposedBridge.log("Something went wrong: " + e + "\nMake a new issue on a Github page.");
             return false;
         } finally {
             if (bufferedReader != null){
@@ -51,6 +57,41 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
         }
     }
 
+    /* public static Boolean lowPower() {
+        Process process = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            process = new ProcessBuilder().command("/system/bin/settings", "get global low_power").start();
+            bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = bufferedReader.readLine();
+            XposedBridge.log("Power Saving Mode check returned " + line);
+            return line.contains("true") || line.contains("1");
+        } catch (Exception e) {
+            XposedBridge.log("Power Saving Mode check returned an exception. Returning fail by default");
+            return false;
+        } finally {
+            if (bufferedReader != null){
+                try {
+                    bufferedReader.close();
+                } catch (IOException ignored) {}
+            }
+            if (process != null){
+                process.destroy();
+            }
+        }
+    } */
+
+    /* public static float alpha(boolean capable) {
+        float defaultA = 0.85f;
+        float supportedA = 0.54f;
+        if (capable) {
+            if (false)
+                return defaultA;
+            else return supportedA;
+        } else return defaultA;
+    } */
+
     @Override
     public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resParam) throws Throwable {
         XModuleResources modRes = XModuleResources.createInstance(rootPackagePath, null);
@@ -62,6 +103,10 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
         if (!lpParam.packageName.equals(rootPackage))
             return;
 
+        if (BuildConfig.DEBUG) {
+            XposedBridge.log("Debug and diagnostics.");
+        }
+
         rootPackagePath = lpParam.appInfo.sourceDir;
 
         supportsBlur = read("ro.surface_flinger.supports_background_blur");
@@ -69,6 +114,9 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
         lowRam = read("ro.config.low_ram");
         disableBlur = read("persist.sysui.disableBlur");
         boolean capable = supportsBlur && !avoidAccel1 && !avoidAccel2 && !lowRam && !disableBlur;
+        if (BuildConfig.DEBUG) XposedBridge.log(capable ?
+                "Your device is capable of handling blur. Using a corresponding alpha value" :
+                "Your device is not capable of handling blur. Using a default alpha value");
         float alpha = capable ? 0.54f : 0.85f;
 
         final Class<?> ScrimController = XposedHelpers.findClass(rootPackage + ".statusbar.phone.ScrimController", lpParam.classLoader);
@@ -80,7 +128,6 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
             }
         };
 
-        Object[] parameterTypesAndCallback;
         if (Build.VERSION.SDK_INT >= 33) { parameterTypesAndCallback = new Object [] {
                 rootPackage + ".statusbar.phone.LightBarController",
                 rootPackage + ".statusbar.phone.DozeParameters",
@@ -93,11 +140,10 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
                 rootPackage + ".statusbar.policy.ConfigurationController",
                 "java.util.concurrent.Executor",
                 rootPackage + ".statusbar.phone.ScreenOffAnimationController",
-                rootPackage + ".statusbar.phone.panelstate.PanelExpansionStateManager",
                 rootPackage + ".keyguard.KeyguardUnlockAnimationController",
                 rootPackage + ".statusbar.phone.StatusBarKeyguardViewManager",
         xcMethodHook};}
-        else { parameterTypesAndCallback = new Object [] {
+        else if (Build.VERSION.SDK_INT == 32) { parameterTypesAndCallback = new Object [] {
                 rootPackage + ".statusbar.phone.LightBarController",
                 rootPackage + ".statusbar.phone.DozeParameters",
                 "android.app.AlarmManager",
@@ -111,7 +157,21 @@ public class Replace implements IXposedHookLoadPackage, IXposedHookInitPackageRe
                 rootPackage + ".statusbar.phone.UnlockedScreenOffAnimationController",
                 rootPackage + ".statusbar.phone.panelstate.PanelExpansionStateManager",
         xcMethodHook};}
+        else if (Build.VERSION.SDK_INT == 31) { parameterTypesAndCallback = new Object [] {
+                rootPackage + ".statusbar.phone.LightBarController",
+                rootPackage + ".statusbar.phone.DozeParameters",
+                "android.app.AlarmManager",
+                rootPackage + ".statusbar.policy.KeyguardStateController",
+                rootPackage + ".util.wakelock.DelayedWakeLock.Builder",
+                "android.os.Handler",
+                "com.android.keyguard.KeyguardUpdateMonitor",
+                rootPackage + ".dock.DockManager",
+                rootPackage + ".statusbar.policy.ConfigurationController",
+                "java.util.concurrent.Executor",
+                rootPackage + ".statusbar.phone.UnlockedScreenOffAnimationController",
+                xcMethodHook};}
 
+        if (BuildConfig.DEBUG) XposedBridge.log("Trying to hook a ScrimController constructor.");
         findAndHookConstructor(ScrimController, parameterTypesAndCallback);
     }
 }
